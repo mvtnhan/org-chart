41670d6c34579144a6653e3bdcf37c41cca92c9a
mvtnhan <mvtnhan@gmail.com>
2022-06-01 19:26:52 +0700

    create org-chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Tree, TreeNode } from 'react-organizational-chart';
+
+import { Data } from './db';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Tree label={<div>{Data.agent_code}</div>}>
+        {Data.agent_childs.map((child) => {
+          return (
+            <TreeNode label={<div>{child.agent_code}</div>}>
+              {child.agent_childs.map((grandChild) => {
+                return (
+                  <TreeNode label={<div>{grandChild.agent_code}</div>}>
+                    {grandChild.agent_childs.map((grandChild2) => {
+                      return (
+                        <TreeNode label={<div>{grandChild2.agent_code}</div>} />
+                      );
+                    })}
+                  </TreeNode>
+                );
+              })}
+            </TreeNode>
+          );
+        })}
+      </Tree>
     </div>
   );
 }
---
+++ src/db.ts
@@ -0,0 +1,56 @@
+export const Data = {
+  agent_code: "A0001",
+  first_name: "Taylor",
+  last_name: "Trinh",
+  agent_id: "M2QxZTYyMDUtNzYxOC00YTBjLWE3YmEtNTAxMjYxMmQ2NDcz",
+  agent_childs: [
+    {
+      agent_code: "A0002",
+      first_name: "Trung",
+      last_name: "Hoang",
+      agent_id: "NjRjNTE1ZDItYTJkMi00MmUyLTgxYzAtNGQ1NDg1NzlhNTUw",
+      agent_childs: [
+        {
+          agent_code: "A0003",
+          first_name: "Edward",
+          last_name: "Ring",
+          agent_id: "MmEzOThlZWYtZDFiNi00ZmU5LTgyYmItMjk0YTNhZTA5OWU4",
+          agent_childs: [
+            {
+              agent_code: "A0007",
+              first_name: "Elves",
+              last_name: "Le",
+              agent_id: "ODMyNzg4YmMtYThhOS00NDQ3LTllMDYtNDYyNzVhODJhOTZh",
+            },
+            {
+              agent_code: "A0008",
+              first_name: "Richard",
+              last_name: "Tran",
+              agent_id: "YWJlNTMxM2ItYTA2OC00YjkxLWEzNDMtNmJkZWY5NmZjZGNh",
+            },
+          ],
+        },
+        {
+          agent_code: "A0004",
+          first_name: "Daniel",
+          last_name: "Ng",
+          agent_id: "NTE3MDJmYmMtNzEzMi00MzJlLWExMWMtZWZlOTk3NTc4YTk4",
+          agent_childs: [
+            {
+              agent_code: "A0005",
+              first_name: "Trang",
+              last_name: "Jane",
+              agent_id: "ZDU0ZDNhNjYtOWVkMy00NjE5LWEwYjMtOTMzN2YwODBmNDY0",
+            },
+            {
+              agent_code: "A0006",
+              first_name: "Robert",
+              last_name: "Jr",
+              agent_id: "M2I4NzcyMzYtOTA3Ny00OGQyLTk2MDItMmRiYmEwYjQzN2Fk",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
